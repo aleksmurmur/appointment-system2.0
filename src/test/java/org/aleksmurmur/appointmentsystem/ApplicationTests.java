d6af4398de3a141794f61787eec7168a0537191d
Alexey Mur <aleksmurmur@yahoo.com>
2023-02-04 16:20:53 +0300

    Init commit

---
+++ src/test/java/org/aleksmurmur/appointmentsystem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aleksmurmur.appointmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
